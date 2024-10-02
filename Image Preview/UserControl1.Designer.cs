8b2e536bca3e85655bc513b04f6c176c52587f20
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-10-02 23:03:21 +0600

    added image previewer

---
+++ Image Preview/UserControl1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Image_Preview
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.DimGray;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(668, 479);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(709, 552);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Image Preview/UserControl1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Image_Preview
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public static string Filepath = @"C:\Users\vn\Desktop";
+        public static string extentions = @".jpg|.png";
+
+
+        public void Reload()
+        {
+            flowLayoutPanel1.Width = this.Width - 20;
+            flowLayoutPanel1.Controls.Clear();//Should be modify
+            if (Directory.Exists(Filepath))
+            {
+                DirectoryInfo dr = new DirectoryInfo(Filepath);
+                FileInfo[] fls = dr.GetFiles();
+
+                string[] exts = extentions.Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < fls.Length; i++)
+                {
+                    for (int j = 0; j < exts.Length; j++)
+                    {
+                        if (fls[i].Extension.ToLower().Contains(exts[j].ToLower()))
+                        {
+                            Controls.mybtn btn = new Controls.mybtn();
+                            btn.btn_text = fls[i].Name;
+                            btn.filepath = fls[i];
+                            btn.refreshme();
+                            flowLayoutPanel1.Controls.Add(btn);
+                        }
+                    }
+                }
+            }
+        }
+        public static void ShowImageDetails(string imagePath)
+        {
+            FileInfo imageFile = new FileInfo(imagePath);
+
+           
+            long fileSize = imageFile.Length;
+            string fileSizeInKB = (fileSize / 1024).ToString() + " KB";
+
+            // Get creation date
+            string creationDate = imageFile.CreationTime.ToString("g");
+
+            // Get image resolution if applicable
+            string resolution = "";
+            if (imageFile.Extension.ToLower() == ".jpg" || imageFile.Extension.ToLower() == ".png")
+            {
+                using (var image = System.Drawing.Image.FromFile(imageFile.FullName))
+                {
+                    resolution = image.Width + "x" + image.Height;
+                }
+            }
+
+            // Display image details in a MessageBox
+            string message = $"File: {imageFile.Name}\n" +
+                            $"Size: {fileSizeInKB}\n" +
+                            $"Resolution: {resolution}\n" +
+                            $"Created On: {creationDate}";
+
+            MessageBox.Show(message, "Image Details", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        
+    }
+   
+}
